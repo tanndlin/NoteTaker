27e899c7c3889890d4168da0e51b4a0d843305ff
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-14 11:34:38 -0500

    Search function

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -14,13 +14,15 @@ type Directory = {
 };
 
 const HomePage = (props: HomePageProps) => {
+    const [searchTerm, setSearchTerm] = React.useState('');
+
     const handleNew = () => {
         const id = props.createNote();
         window.location.href = `/${id}/edit`;
     };
 
     // This will display notes in a folder heirarchy according to their directory
-    const getHeirarchy = (notes: Note[]) => {
+    const getHeirarchy = (notes: Note[], searchTerm: string) => {
         const heirarchy: Directory = { notes: [], dirs: {} };
         notes.forEach((note) => {
             const dir = note.directory;
@@ -33,10 +35,30 @@ const HomePage = (props: HomePageProps) => {
                 currentDir = currentDir.dirs[dir];
             });
 
-            currentDir.notes.push(note);
+            if (
+                !searchTerm ||
+                note.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+                currentDir.notes.push(note);
+            }
         });
 
-        return heirarchy;
+        // Prune empty directories
+        const prune = (dir: Directory) => {
+            Object.keys(dir.dirs).forEach((key) => {
+                dir.dirs[key] = prune(dir.dirs[key]);
+                if (
+                    dir.dirs[key].notes.length === 0 &&
+                    Object.keys(dir.dirs[key].dirs).length === 0
+                ) {
+                    delete dir.dirs[key];
+                }
+            });
+
+            return dir;
+        };
+
+        return prune(heirarchy);
     };
 
     // Recursively create the file structure
@@ -61,16 +83,18 @@ const HomePage = (props: HomePageProps) => {
                 <h1 className="text-4xl font-bold">Home</h1>
                 <button onClick={handleNew}>New Note</button>
             </header>
-            <article className="bg-secondary w-full min-h-1/2 my-auto p-4">
+            <article className="bg-secondary w-full min-h-1/2 my-auto p-4 rounded-md">
                 <header className="flex justify-between mb-8">
                     <h2 className="text-xl">Notes</h2>
                     <input
                         className="rounded-md px-2 py-1"
                         type="text"
                         placeholder="Search"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
                 </header>
-                {createFileStructure(getHeirarchy(props.notes), 0)}
+                {createFileStructure(getHeirarchy(props.notes, searchTerm), 0)}
             </article>
         </main>
     );
