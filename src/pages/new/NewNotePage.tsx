07b769bd991a31889a0a2745d4740d204e3a77a9
Tanndlin <tanndlin@gmail.com>
2022-12-13 23:26:59 -0500

    New Note Styling

--- src/pages/new/NewNotePage.tsx
+++ src/pages/new/NewNotePage.tsx
@@ -13,14 +13,14 @@ How about now a list view
     );
 
     return (
-        <div className="px-4 h-full w-screen flex flex-col flex-grow">
-            <h1 className="text-3xl mb-8 ">Title</h1>
+        <div className="px-8 h-full w-screen flex flex-col flex-grow">
+            <h1 className="text-3xl mb-8">Title</h1>
             <div className="flex flex-1 w-full">
-                <section className="w-1/2 mx-4 h-full flex flex-col flex-grow">
+                <section className="w-1/2 mr-4 h-full flex flex-col flex-grow">
                     <h2 className="text-2xl text-center w-full mb-8">Edit</h2>
                     <Edit {...{ markdown, setMarkdown }} />
                 </section>
-                <section className="w-1/2 mr-4 pl-4 border-l-2 border-tertiary flex flex-col flex-grow">
+                <section className="w-1/2 pl-4 border-l-2 border-tertiary flex flex-col flex-grow">
                     <h2 className="text-2xl text-center w-full mb-8">
                         Preview
                     </h2>
