2884ad6037c461343fbce02c1e5c121a33d064d8
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-15 15:13:26 -0500

    Allow nested ul

--- src/common/Preview/MarkdownRenderer.tsx
+++ src/common/Preview/MarkdownRenderer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import 'github-markdown-css';
+import './Markdown.scss';
 
 type MarkdownRendererProps = {
     markdown: string;
