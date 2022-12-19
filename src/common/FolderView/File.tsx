6ac90fa462e0238e0bf7a295df85a782971c54fd
Tanner Sandlin <tanner.sandlin@gtri.gatech.edu>
2022-12-19 12:11:11 -0500

    Show Magnifying glass on file hover

--- src/common/FolderView/File.tsx
+++ src/common/FolderView/File.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Note } from '../types';
+import MagnifyingGlassIcon from '../Icons/MagnifyingGlassIcon';
 
 type FileProps = {
     note: Note;
@@ -9,8 +10,9 @@ export const File = (props: FileProps) => {
     const { note } = props;
 
     return (
-        <li key={note.id} className="ml-8">
+        <li key={note.id} className="file ml-8 flex">
             <a href={`/${note.id}`}>{note.title}</a>
+            <MagnifyingGlassIcon className="w-4" />
         </li>
     );
 };
